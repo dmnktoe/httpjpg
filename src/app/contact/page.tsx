a26b6c5a1a460a095de4008a861f756098d7f517
Domenik Töfflinger <domenikt96@yahoo.de>
2024-02-04 22:51:27 +0100

    chore: create contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+import { Container } from '@/components/layout/Container';
+
+export default function ContactPage() {
+  return (
+    <main>
+      <section>
+        <Container className='py-16'>contact</Container>
+      </section>
+    </main>
+  );
+}

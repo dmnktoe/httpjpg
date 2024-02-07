9a1bc3f979c67149c544d77ddd4ab87863bd4569
Domenik Töfflinger <domenikt96@yahoo.de>
2024-02-07 18:25:00 +0100

    feat: add glossy background images

--- src/components/templates/project-list.tsx
+++ src/components/templates/project-list.tsx
@@ -12,21 +12,21 @@ const ProjectCard = ({ project }: { project: Project }) => {
         <Slideshow images={project.images} />
       </div>
       <div>
-        <div className='flex flex-row gap-2'>
-          <div className='w-1/2 text-[6.5vw] leading-[0.8] tracking-tighter'>
+        <div className='flex flex-col gap-2 xl:flex-row'>
+          <div className='w-full bg-[url("/images/unnamed.gif")] text-[8.5vw] leading-[0.8] tracking-tighter xl:w-1/2 xl:text-[6.5vw]'>
             <div className='inline-block'>
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 viewBox='0 0 32 32'
-                className='w-[5vw] rotate-90'
-                fill='#000000'
+                className='w-[6vw] rotate-90 xl:w-[5vw]'
+                fill='currentColor'
               >
                 <path d='m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z' />
               </svg>
             </div>
             {project.name}
           </div>
-          <div className='w-1/2 text-xs tracking-tighter'>
+          <div className='line-clamp-6 w-full text-xs tracking-tighter xl:line-clamp-none xl:w-1/2'>
             {project.description}
             <p className='text-[9px] tracking-tighter text-neutral-400'>
               Fotos: Dominik Müller

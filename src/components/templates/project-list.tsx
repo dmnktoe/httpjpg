c161ee2787cff66a5ecb34179b74c5fedd45e112
Domenik Töfflinger <domenikt96@yahoo.de>
2024-02-10 20:50:17 +0100

    refactor: separate project-card from project-list

--- src/components/templates/project-list.tsx
+++ src/components/templates/project-list.tsx
@@ -1,52 +1,9 @@
 import * as React from 'react';
 
 import { Container } from '@/components/layout/container';
-import UnstyledLink from '@/components/ui/links/UnstyledLink';
-import Slideshow from '@/components/ui/slideshow';
+import { ProjectCard } from '@/components/templates/project-card';
 
 import { Project } from '@/interfaces/Project';
-
-const ProjectCard = ({ project }: { project: Project }) => {
-  return (
-    <div className='flex flex-col gap-2'>
-      <div className='-mx-2 md:mx-0 xl:px-48'>
-        <Slideshow images={project.images} />
-      </div>
-      <div>
-        <div className='flex flex-col gap-2 xl:flex-row'>
-          <div className='w-full overflow-hidden rounded-full border-[1px] border-solid border-black bg-[url("/images/unnamed.gif")] text-[8.5vw] leading-[0.8] tracking-tighter xl:w-1/2 xl:text-[6.5vw]'>
-            <div className='inline-block'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 32 32'
-                className='w-[6vw] rotate-90 xl:w-[5vw]'
-                fill='currentColor'
-              >
-                <path d='m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z' />
-              </svg>
-            </div>
-            {project.name}
-          </div>
-          <div className='project-desc h-auto w-full rounded-[10vw] border-[1px] border-solid border-black bg-white/70 p-1 text-xs tracking-tighter text-black xl:w-1/2 xl:rounded-full xl:p-6'>
-            <div className='mx-auto w-10/12'>
-              2022.10.23
-              <br />
-              {project.description}
-              <br />
-              <br />
-              <UnstyledLink
-                href={`/${project.slug}`}
-                className='hover:text-primary-600 line-clamp-1 text-[blue] hover:underline'
-              >
-                -̸̨̱̠̳̩̼͙̈̀̀̄̃̆́͠ͅ↳↳↳{project.slug}↳↳↳
-              </UnstyledLink>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export const ProjectList = ({ projects }: { projects: Project[] }) => {
   return (

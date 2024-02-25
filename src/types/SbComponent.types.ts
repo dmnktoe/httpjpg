957e46250e0e5fd06ce1ba36b0d44de0480b7490
Domenik Töfflinger <domenikt96@yahoo.de>
2024-02-25 23:19:06 +0100

    chore: eslint-disable-next-line @typescript-eslint/no-explicit-any

--- src/types/SbComponent.types.ts
+++ src/types/SbComponent.types.ts
@@ -5,6 +5,7 @@ export interface ContainerStoryblok {
   width?: 'full' | 'container';
   _uid: string;
   component: 'container';
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [k: string]: any;
 }
 
@@ -13,10 +14,12 @@ export interface PageStoryblok {
   seoTitle?: string;
   title?: string;
   seoDescription?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   seo?: any;
   _uid: string;
   component: 'page';
   uuid?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [k: string]: any;
 }
 
@@ -27,6 +30,7 @@ export type MultiassetStoryblok = {
   filename: string;
   name: string;
   title?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [k: string]: any;
 }[];
 
@@ -34,5 +38,6 @@ export interface SlideshowStoryblok {
   images: MultiassetStoryblok;
   _uid: string;
   component: 'slideshow';
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [k: string]: any;
 }

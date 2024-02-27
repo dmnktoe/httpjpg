41e132c9dc964e93c1697e1d25af97f2f5aa5886
Domenik Töfflinger <domenikt96@yahoo.de>
2024-02-27 16:14:12 +0100

    chore: update `NextImage` JSDoc

--- src/components/ui/NextImage.tsx
+++ src/components/ui/NextImage.tsx
@@ -17,9 +17,16 @@ type NextImageProps = {
   ImageProps;
 
 /**
- *
+ * Next.js Image component with skeleton loading
  * @description Must set width using `w-` className
  * @param useSkeleton add background with pulse animation, don't use it if image is transparent
+ * @param src
+ * @param width
+ * @param height
+ * @param alt
+ * @param className
+ * @param classNames
+ * @param rest
  */
 export default function NextImage({
   useSkeleton = false,

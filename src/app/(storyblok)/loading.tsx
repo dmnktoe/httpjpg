275e750af2d85cccc869691f9c3f50683e119da1
Domenik Töfflinger <domenikt96@yahoo.de>
2024-04-13 20:07:54 +0200

    feat: new loading indicator, remove skeleton

--- src/app/(storyblok)/loading.tsx
+++ src/app/(storyblok)/loading.tsx
@@ -1,29 +1,10 @@
-import DynamicLoad from 'next/dynamic';
-
 import { Container } from '@/components/layout/Container';
-import { Flexbox } from '@/components/layout/Flexbox';
-const Skeleton = DynamicLoad(() => import('react-loading-skeleton'), {
-  ssr: false,
-});
 
 const Loading = () => {
   return (
     <main>
       <Container width='container'>
-        <Flexbox gap direction='row'>
-          <Skeleton
-            height={200}
-            highlightColor='rgba(255,255,255,0.2)'
-            baseColor='rgba(0,0,0,0.1)'
-            containerClassName='w-full'
-          />
-          <Skeleton
-            height={200}
-            highlightColor='rgba(255,255,255,0.2)'
-            baseColor='rgba(0,0,0,0.1)'
-            containerClassName='w-full'
-          />
-        </Flexbox>
+        <span className='animate-rainbow'>Loading...</span>
       </Container>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,10 +54,49 @@ export default {
             backgroundPosition: '700px 0',
           },
         },
+        rainbow: {
+          '100%, 0%': {
+            color: 'rgb(255,0,0)',
+          },
+          '8%': {
+            color: 'rgb(255,127,0)',
+          },
+          '16%': {
+            color: 'rgb(255,255,0)',
+          },
+          '25%': {
+            color: 'rgb(127,255,0)',
+          },
+          '33%': {
+            color: 'rgb(0,255,0)',
+          },
+          '41%': {
+            color: 'rgb(0,255,127)',
+          },
+          '50%': {
+            color: 'rgb(0,255,255)',
+          },
+          '58%': {
+            color: 'rgb(0,127,255)',
+          },
+          '66%': {
+            color: 'rgb(0,0,255)',
+          },
+          '75%': {
+            color: 'rgb(127,0,255)',
+          },
+          '83%': {
+            color: 'rgb(255,0,255)',
+          },
+          '91%': {
+            color: 'rgb(255,0,127)',
+          },
+        },
       },
       animation: {
         flicker: 'flicker 3s linear infinite',
         shimmer: 'shimmer 1.3s linear infinite',
+        rainbow: 'rainbow 2.5s linear infinite',
       },
     },
   },
